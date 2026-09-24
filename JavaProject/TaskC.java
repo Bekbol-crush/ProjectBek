c344a3bc1a11497b3f08e05f82e26b9f95240fda
Student <student@gmail.com>
2026-09-24 08:56:14 +0600

    Дележ яблок-1

--- JavaProject/TaskC.java
+++ JavaProject/TaskC.java
@@ -1,7 +1,7 @@
-public class TaskC
+
 import java.util.Scanner;
 
-public class Main {
+public class TaskC {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
